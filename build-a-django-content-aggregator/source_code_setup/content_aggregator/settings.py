e278f962114ad86824295c81cfd90556a8c7ef76
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2021-11-20 20:18:13 +0100

    Creating HomePageView

--- build-a-django-content-aggregator/source_code_setup/content_aggregator/settings.py
+++ build-a-django-content-aggregator/source_code_setup/content_aggregator/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'content_aggregator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
--- build-a-django-content-aggregator/source_code_setup/podcasts/views.py
+++ build-a-django-content-aggregator/source_code_setup/podcasts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from .models import Episode
+from typing import Dict, Any
+
 # Create your views here.
+class HomePageView(ListView):
+    template_name = 'homepage.html'
+    model = Episode
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['episodes'] = self.model.objects.filter().order_by('-pub_date')[:10]
+
+        return context
