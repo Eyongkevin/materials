da34c3d27403b7fda73caa95aa15bc2280d6cf0d
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2021-11-20 22:03:15 +0100

    refactor to handle two RSS

--- build-a-django-content-aggregator/source_code_setup/podcasts/management/commands/startjobs.py
+++ build-a-django-content-aggregator/source_code_setup/podcasts/management/commands/startjobs.py
@@ -4,22 +4,41 @@ from dateutil import parser
 from podcasts.models import Episode
 from typing import Any, Optional
 
+RSS_CONTAINER  = {}
+
+def register_rss(rss):
+    RSS_CONTAINER[rss.__name__] = rss
+    return rss
+
+def save_new_feeds(feed):
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
+
+    for item in feed.entries:
+        if not Episode.objects.filter(guid=item.guid).exists():
+            episode = Episode(
+                title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid,
+            )
+            episode.save()
+
+@register_rss
+def real_python_rss():
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_feeds(_feed)
+
+@register_rss
+def talk_python_rss():
+    _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
+    save_new_feeds(_feed)
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
-        feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image["href"]
-
-        for item in feed.entries:
-            if not Episode.objects.filter(guid=item.guid).exists():
-                episode = Episode(
-                    title=item.title,
-                    description=item.description,
-                    pub_date=parser.parse(item.published),
-                    link=item.link,
-                    image=podcast_image,
-                    podcast_name=podcast_title,
-                    guid=item.guid,
-                )
-                episode.save()
+        for rss in RSS_CONTAINER:
+            RSS_CONTAINER[rss]()
+        
